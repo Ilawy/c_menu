739cc84eccaafe0e1a241e39f8b52a8fb7a49237
ilawy <mm.ilawy@gmail.com>
2024-11-22 11:25:42 +0200

    added comment

--- inputter.h
+++ inputter.h
@@ -10,9 +10,23 @@
 #define USABLE_START 32
 #define USABLE_END 126
 
+#define ESC_KEY 27
+#define ARROW_RIGHT 67
+#define ARROW_LEFT 68
+#define ARROW_UP 65
+#define ARROW_DOWN 66
+
+#define SQBR_LEFT 91
+
 #define ENTER_KEY 10
 #define MINUS_KEY 45
 #define BACKSPACE_KEY 127
+
+typedef enum InputRetCode
+{
+    Success = 0,
+    Cancel = 1
+};
 
 int prompt_int(char *message)
 {
@@ -54,16 +68,46 @@ int prompt_int(char *message)
     }
 }
 
+// NOT YET READY
 prompt_string(char message[], int size, char target[])
 {
     printf("%s", message);
     int index = 0;
+    int cursor = 0;
     int done = 0;
+    int base_x, base_y;
+    wherexy(&base_x, &base_y);
     while (!done)
     {
         char hit = getch();
         switch (hit)
         {
+        case ESC_KEY:
+            if (kbhit() == 0)
+                return Cancel;
+            else if (getch() == SQBR_LEFT)
+            {
+                // extended
+                char special = getch();
+                switch (special)
+                {
+                case ARROW_LEFT:
+                    if (cursor > 0)
+                    {
+                        cursor--;
+                        gotoxy(base_x + cursor, base_y);
+                    }
+                    break;
+                case ARROW_RIGHT:
+                    if (cursor < index)
+                    {
+                        cursor++;
+                        gotoxy(base_x + cursor, base_y);
+                    }
+                    break;
+                }
+            }
+            break;
         case USABLE_START ... USABLE_END:
             if (index < size - 1)
             {
@@ -79,6 +123,11 @@ prompt_string(char message[], int size, char target[])
                 gotox(wherex() - 1);
                 target[index] = 0;
                 index--;
+                if (cursor > 0)
+                {
+                    cursor--;
+                    gotoxy(base_x + cursor, base_y);
+                }
             }
             break;
         case ENTER_KEY:
